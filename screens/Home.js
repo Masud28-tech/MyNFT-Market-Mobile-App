97bb06615d083829bbe298d1ac711a1e9433d8db
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2023-01-02 15:00:03 +0530

    Commit 04: Day2 --> Implemented FlatList i.e. List of cards with cardData in home screen.

--- screens/Home.js
+++ screens/Home.js
@@ -7,8 +7,22 @@ const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusedStatusBar background={COLORS.primary} />
-      <View>
-      <Text>Hello</Text>
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={NFTData} />}
+            keyExtractor={(item) => item.id}
+            ListHeaderComponent={<HomeHeader />}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
+
+      <View style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, zIndex: -1 }}>
+        <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+        <View style={{ flex: 1, backgroundColor: COLORS.white }} />
       </View>
     </SafeAreaView>
   )
