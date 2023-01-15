cb2825e196513a6fd3c5f232eefaefb9a9da7fb9
Masud Ansari <65458743+Masud28-tech@users.noreply.github.com>
2023-01-15 13:27:10 +0530

    Commit 11: Day 7 --> Work on Details Screen: Create 'Place bid' button at the end of the screen and set screen status bar.

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import NFTCard from "./NFTCard";
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsBid from "./DetailsBid";
+import DetailsHeader from "./DetailsHeader";
 
-export {NFTCard, HomeHeader, FocusedStatusBar};
+export { NFTCard, HomeHeader, FocusedStatusBar, DetailsBid, DetailsHeader };
--- screens/Details.js
+++ screens/Details.js
@@ -1,8 +1,56 @@
-import { Text } from "react-native"
+import React from "react";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
 
-const Details = () => {
+import { FocusedStatusBar, DetailsBid, DetailsHeader } from '../components';
+import { ReactButton } from "../components/Buttons";
+import { SHADOWS, SIZES } from "../constants";
+
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
   return (
-    <Text>Details</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+
+      <View style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: SIZES.font,
+        zIndex: 1,
+        backgroundColor: "rgba(255, 255, 255, 0.5)"
+      }}>
+
+        <ReactButton
+          minWidth={170}
+          fontSize={SIZES.large}
+          {...SHADOWS.dark}
+        />
+
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={(item) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+            <DetailsHeader />
+          </React.Fragment>
+        )}
+      />
+
+    </SafeAreaView>
   )
 }
 
